01ad09624408d38878bccb67eae85ec418c53a65
Kishore S <65441464+kichulu@users.noreply.github.com>
2021-03-08 18:51:18 +0530

    Initial Commit

---
+++ KAnimatedStack/KAnimatedStack.h
@@ -0,0 +1,18 @@
+//
+//  KAnimatedStack.h
+//  KAnimatedStack
+//
+//  Created by Kishore S on 08/03/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for KAnimatedStack.
+FOUNDATION_EXPORT double KAnimatedStackVersionNumber;
+
+//! Project version string for KAnimatedStack.
+FOUNDATION_EXPORT const unsigned char KAnimatedStackVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KAnimatedStack/PublicHeader.h>
+
+
